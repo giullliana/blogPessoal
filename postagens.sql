3b097f18c974a9cc3be7857a2e6d4d9c32e77bd5
Giulliana Munhoz <giulli_munhoz@hotmail.com>
2021-03-18 15:20:51 -0300

    Blogpessoal 2

---
+++ postagens.sql
@@ -0,0 +1,3 @@
+insert into postagens(date,texto,titulo)value 
+("2020-04-30  10.10.10.000","Estou aprendendo","API rest spring");
+select*from postagens;
--- src/main/java/org/generation/blogpessoal2/model/Postagem.java
+++ src/main/java/org/generation/blogpessoal2/model/Postagem.java
@@ -22,6 +22,7 @@ private long id;
 	
 	@NotNull
 
+
 private String titulo;
 	
 	@NotNull
