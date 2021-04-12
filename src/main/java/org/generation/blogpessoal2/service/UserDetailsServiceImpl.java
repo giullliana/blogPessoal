8a8ef2aa7ebffbdc46cfa78ea02cce23745b0231
Giulliana Munhoz <giulli_munhoz@hotmail.com>
2021-04-12 14:22:00 -0300

    Deploy e implementando swagger

--- src/main/java/org/generation/blogpessoal2/service/UserDetailsServiceImpl.java
+++ src/main/java/org/generation/blogpessoal2/service/UserDetailsServiceImpl.java
@@ -1,26 +1,28 @@
-package org.generation.blogpessoal2.seguranca;
+package org.generation.blogpessoal2.service;
+
+import java.util.Optional;
 
 import org.generation.blogpessoal2.model.Usuario;
 import org.generation.blogpessoal2.repository.UsuarioRepository;
+import org.generation.blogpessoal2.seguranca.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.sun.el.stream.Optional;
-
 @Service
-
 public class UserDetailsServiceImpl implements UserDetailsService {
-@Autowired
-private  UsuarioRepository userRepository;
 
-@Override
-public UserDetailsImpl loadUserByUsername(String userName) {
-	java.util.Optional<Usuario> user =userRepository.findByUsuario(userName);
-	user.orElseThrow(() -> new UsernameNotFoundException(userName + "not found."));
+	@Autowired
+	private UsuarioRepository userRepository;
 	
-	return user.map(UserDetailsImpl:: new).get();
-}
+	@Override
+	public UserDetails loadUserByUsername(String userName) {
+		Optional<Usuario> user =userRepository.findByUsuario(userName);
+		user.orElseThrow(() -> new UsernameNotFoundException(userName + "not found"));
+		return user.map(UserDetailsImpl :: new).get();
+		
+		
+	}
 }
